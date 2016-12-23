cdf92b4e5988ef439251761b0ef722a2bc983b23
James Rowley <james@jrowley.me>
2016-12-22 16:55:47 -0800

    Set up Xamarin project

---
+++ LiteID/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace LiteID
+{
+    [Activity(Label = "LiteID", MainLauncher = true, Icon = "@drawable/icon4sc_256")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ LiteID/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+[assembly: AssemblyTitle("LiteID")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LiteID Team")]
+[assembly: AssemblyProduct("LiteID-Android")]
+[assembly: AssemblyCopyright("Copyright 2016 LiteID Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
