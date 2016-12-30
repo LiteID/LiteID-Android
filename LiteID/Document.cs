a4ab5d7d9fce9deec4d1f577fe0053c436f46dc1
James Rowley <james@jrowley.me>
2016-12-29 23:26:24 -0600

    Basic implementation of Document and DocumentList

---
+++ LiteID/Document.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+//Placeholder class for the hash of a document
+//Constructor initializes 32 bytes of random numbers
+//ToString returns as hex string
+public class BHash
+{
+    [XmlIgnore]
+    public Byte[] HashData = new Byte[32];
+    public BHash()
+    {
+        Random random = new Random();
+        random.NextBytes(HashData);
+    }
+    public override string ToString()
+    {
+        StringBuilder hex = new StringBuilder(HashData.Length * 2);
+        foreach (byte b in HashData) hex.AppendFormat("{0:x2}", b);
+        return hex.ToString();
+    }
+}
+
+[Serializable]
+public class Document
+{
+    [XmlAttribute]
+    public string ID;
+    [XmlIgnore]
+    public BHash Hash;
+    public string Name;
+    //TODO: Original file name or extension?
+    public DateTime IngestionTime;
+
+    //Ingest file as document
+    public void IngestDocument(Uri URI)
+    {
+
+    }
+
+    //Ingest string as document
+    public void IngestDocument(string TextDoc)
+    {
+
+    }
+
+    //Initialize document with random data
+    public void RandomDocument(Random gen)
+    {
+        Hash = new BHash();
+        ID = Hash.ToString();
+
+        string[] names = { "Document", "Work Thing", "Name", "ID", "Video", "Picture", "Evidence", "Incriminating Record", "Stolen SSN", "Unencrypted Password", "Unfinished App", "Joke Text", "Contract", "Loan" };
+        Name = names.PickRandom();
+        Name += " #" + gen.Next(100);
+
+        DateTime start = new DateTime(2016, 1, 1);
+        //int range = (DateTime.Today - start).Days;
+        int range = 365;
+        IngestionTime = start.AddDays(gen.Next(range));
+    }
+}
+
+public class DocumentList
+{
+    public List<Document> Documents = new List<Document>();
+    private XmlSerializer dlser = new XmlSerializer(typeof(List<Document>), "Documents");
+
+    public DocumentList()
+    {
+
+    }
+
+    public DocumentList(string filepath)
+    {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        string filename = Path.Combine(path, filepath);
+        TextReader reader = new StreamReader(filename);
+        Documents = (List<Document>)dlser.Deserialize(reader);
+        reader.Close();
+    }
+
+    public void SaveList(string filepath)
+    {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        string filename = Path.Combine(path, filepath);
+        TextWriter writer = new StreamWriter(filename, false);
+        dlser.Serialize(writer, Documents);
+        writer.Close();
+    }
+
+    public void Randomize()
+    {
+        Documents.Clear();
+        Random random = new Random();
+        int count = random.Next(30);
+        for (int i = 0; i < count; i++)
+        {
+            Document inter = new Document();
+            inter.RandomDocument(random);
+            Documents.Add(inter);
+        }
+    }
+}
--- LiteID/MainActivity.cs
+++ LiteID/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace LiteID
 {
-    [Activity(Label = "LiteID", MainLauncher = true, Icon = "@drawable/icon4sc_256")]
+    [Activity(Label = "LiteID", MainLauncher = true, Icon = "@drawable/icon4sc_256", Theme = "@android:style/Theme.DeviceDefault.Light.NoActionBar")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
@@ -12,6 +12,50 @@ namespace LiteID
             base.OnCreate(bundle);
 
             SetContentView (Resource.Layout.Main);
+            Button randomize = FindViewById<Button>(Resource.Id.button1);
+            Button clearlist = FindViewById<Button>(Resource.Id.button4);
+            Button writeout = FindViewById<Button>(Resource.Id.button2);
+            Button readin = FindViewById<Button>(Resource.Id.button3);
+
+            DocumentList mainlist = new DocumentList();
+
+            randomize.Click += delegate
+            {
+                mainlist.Randomize();
+                UpdateList(mainlist);
+            };
+
+            clearlist.Click += delegate
+            {
+                mainlist.Documents.Clear();
+                UpdateList(mainlist);
+            };
+
+            writeout.Click += delegate
+            {
+                mainlist.SaveList("documents.lxm");
+                UpdateList(mainlist);
+            };
+
+            readin.Click += delegate
+            {
+                mainlist = new DocumentList("documents.lxm");
+                UpdateList(mainlist);
+            };
+        }
+
+        private void UpdateList(DocumentList list)
+        {
+            LinearLayout listview = FindViewById<LinearLayout>(Resource.Id.listView);
+            listview.RemoveAllViews();
+
+            foreach (Document doc in list.Documents)
+            {
+                TextView label = new TextView(this.ApplicationContext);
+                label.Text = doc.Name;
+                label.SetTextColor(Android.Graphics.Color.Black);
+                listview.AddView(label);
+            }
         }
     }
 }
