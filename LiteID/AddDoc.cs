16da9edd36e2fb71f94908617ba1dac520c8f5e2
James Rowley <james@jrowley.me>
2017-01-02 22:21:23 -0800

    Interface for adding document

--- LiteID/AddDoc.cs
+++ LiteID/AddDoc.cs
@@ -13,7 +13,7 @@ using Android.Util;
 
 namespace LiteID
 {
-    [Activity(Label = "Add New Document")]
+    [Activity(Label = "Add New Document", WindowSoftInputMode = SoftInput.AdjustPan)]
     [IntentFilter(new[] { Intent.ActionSend }, Categories = new[] {
         Intent.CategoryDefault,
         Intent.CategoryBrowsable,
@@ -34,9 +34,40 @@ namespace LiteID
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.AddDoc);
+            EditText textTitle = FindViewById<EditText>(Resource.Id.textTitle);
+            RadioButton radioFile = FindViewById<RadioButton>(Resource.Id.radioFile);
+            Button buttonFile = FindViewById<Button>(Resource.Id.buttonFile);
+            RadioButton radioText = FindViewById<RadioButton>(Resource.Id.radioText);
+            EditText textContent = FindViewById<EditText>(Resource.Id.textContent);
+            Button buttonCreate = FindViewById<Button>(Resource.Id.buttonCreate);
+            Button buttonDiscard = FindViewById<Button>(Resource.Id.buttonDiscard);
 
-            Log.Info("intent", Intent.HasExtra(Intent.ExtraText) ? Intent.GetStringExtra(Intent.ExtraText) : "fuck you");
+            radioFile.Click += delegate
+            {
+                radioText.Checked = false;
+            };
 
+            buttonFile.Click += delegate
+            {
+                Intent getFileIntent = new Intent(Intent.ActionGetContent);
+                getFileIntent.SetType("*/*");
+                StartActivityForResult(getFileIntent, 0);
+            };
+
+            radioText.Click += delegate
+            {
+                radioFile.Checked = false;
+            };
+
+            buttonCreate.Click += delegate
+            {
+                Finish();
+            };
+
+            buttonDiscard.Click += delegate
+            {
+                Finish();
+            };
         }
     }
 }
